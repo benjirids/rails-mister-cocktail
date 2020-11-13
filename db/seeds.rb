85f132e88189663f229b715829f7a7c08dd0c266
Ben Ridley <benridley3@gmail.com>
2020-11-13 15:36:09 +0000

    added precompile fixer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-20.times do
-  Ingredient.create!(name: Faker::Movies::LordOfTheRings.character)
+
+# 20.times do
+#   Ingredient.create!(name: Faker::Movies::LordOfTheRings.character)
+# end
+
+require 'open-uri'
+require 'json'
+url = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+json = JSON.parse(url.read)
+json['drinks'].each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+  puts "Creating ingredient"
 end
+puts "Finished"
